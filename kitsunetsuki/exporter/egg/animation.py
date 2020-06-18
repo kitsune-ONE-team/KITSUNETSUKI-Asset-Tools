af435033f740553ba6893eeb3eaf17c739dcf998
Yonnji <yonnji@kitsune.one>
2020-06-18 22:52:32 +0300

    cardmaker initial

--- kitsunetsuki/exporter/egg/animation.py
+++ kitsunetsuki/exporter/egg/animation.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 kitsune.ONE team.
+# Copyright (c) 2020 kitsune.ONE team.
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
